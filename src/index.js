59de2f45a44990c5fd50b45175ee122dc3a337fa
Shiyu Wang <shiyu3169@gmail.com>
2018-09-04 13:43:30 -0400

    redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux';
+import {Provider, connect} from 'react-redux'
 
 const HelloWorld = ({message}) => 
-<div>
-    <h1>Hello World</h1>
-    <h2>{message}</h2>
-</div>
+    <div>
+        <h1>Hello World</h1>
+        <h2>{message}</h2>
+    </div>
+
+const stateToPropertyMapper = state => ({
+        message: state.message 
+    })
+
+const HelloContainer = connect(stateToPropertyMapper)(HelloWorld)
+
+let initialState = {
+    message: 'Carpe diem'
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <HelloWorld message="Life is good!"/>, document.getElementById('root')
+    <Provider store={store}>
+        <HelloContainer/>
+    </Provider>, document.getElementById('root')
+   
 )
